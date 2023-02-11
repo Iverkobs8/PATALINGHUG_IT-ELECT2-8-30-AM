ce0ebcf0417294be2091bf2caa3dfd6b7105544e
Iverkob8 <kobepatalinghugiverson17@gmail.com>
2023-02-11 12:59:33 +0800

    AndroidStudio

--- app/src/main/java/com/example/kobeproject/MainActivity.java
+++ app/src/main/java/com/example/kobeproject/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.kobeproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose log.");
+        Log.d(TAG, "This is a debug log.");
+        Log.i(TAG, "This is an info log.");
+        Log.w(TAG, "This is a warn log.");
+        Log.e(TAG, "This is an error log.");
     }
 }
