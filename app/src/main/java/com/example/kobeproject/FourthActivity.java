94a8e68918de0fe89ae985fd34ec73d529eae4c2
Ivekobs8 <kobepatalinghugiverson17@gmail.com>
2023-03-04 12:48:51 +0800

    input

--- app/src/main/java/com/example/kobeproject/FourthActivity.java
+++ app/src/main/java/com/example/kobeproject/FourthActivity.java
@@ -2,7 +2,10 @@ package com.example.kobeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FourthActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class FourthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth2);
+
+        Button fourthbutton = (Button) findViewById(R.id.button4);
+        fourthbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FourthActivity.this, ActivityFifth.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
+
+
