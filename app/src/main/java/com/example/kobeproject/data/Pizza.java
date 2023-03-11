52ba019f831c1e35f94122c162153166feaa0f9a
Iversonkobs8 <kobepatalinghugiverson17@gmail.com>
2023-03-11 13:25:46 +0800

    Updated Pizza packages

---
+++ app/src/main/java/com/example/kobeproject/data/Pizza.java
@@ -0,0 +1,4 @@
+package com.example.kobeproject.data;
+
+public class Pizza {
+}
--- app/src/main/java/com/example/kobeproject/fragments/PizzaDetailFragment.java
+++ app/src/main/java/com/example/kobeproject/fragments/PizzaDetailFragment.java
@@ -1,4 +1,4 @@
-package com.example.kobeproject;
+package com.example.kobeproject.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.kobeproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
