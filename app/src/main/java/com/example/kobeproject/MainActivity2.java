0d639fc15f0dbef8e6b5fce40c8677b25d39e8ae
Iversonkobs8 <kobepatalinghugiverson17@gmail.com>
2023-03-11 11:41:22 +0800

    Fragment A and B with activity button

--- app/src/main/java/com/example/kobeproject/MainActivity2.java
+++ app/src/main/java/com/example/kobeproject/MainActivity2.java
@@ -2,13 +2,53 @@ package com.example.kobeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity2 extends AppCompatActivity {
-
+    Button firstFragment, secondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        // get the reference of Buttons
+        firstFragment = (Button) findViewById(R.id.button6);
+        secondFragment = (Button) findViewById(R.id.button7);
+
+
+        //performs setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        secondFragment.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        }));
+    }
+    private void loadFragment(Fragment fragment)
+    {
+        FragmentManager fm = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        fragmentTransaction.replace(R.id.FrameLayout);
+        fragmentTransaction.commit();
+
+
+
+
     }
 }
